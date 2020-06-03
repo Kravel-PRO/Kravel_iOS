2a3839c849729e026fd3c391aaccc0f2a959de6e
dongminyoon <ehdals8965@naver.com>
2020-06-03 16:13:48 +0900

    setting: 프로젝트 초기 설정

---
+++ Kravel/Kravel/Sources/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kravel
+//
+//  Created by 윤동민 on 2020/06/03.
+//  Copyright © 2020 윤동민. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
