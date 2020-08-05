e93cdfac8f8059eeab8cf279f081291ffb1103d7
dongminyoon <ehdals8965@naver.com>
2020-08-05 19:37:50 +0900

    feat: Home Tab 인기있는 장소, 포토리뷰 부분 구현

---
+++ Kravel/Kravel/Sources/View/CollectionViewCell/HotPlaceCell.swift
@@ -0,0 +1,21 @@
+//
+//  HotPlaceCell.swift
+//  Kravel
+//
+//  Created by 윤동민 on 2020/08/05.
+//  Copyright © 2020 윤동민. All rights reserved.
+//
+
+import UIKit
+
+class HotPlaceCell: UICollectionViewCell {
+    static let identifier = "HotPlaceCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
--- Kravel/Kravel/Sources/ViewController/HomeVC.swift
+++ Kravel/Kravel/Sources/ViewController/HomeVC.swift
@@ -33,6 +33,15 @@ class HomeVC: UIViewController {
         }
     }
     
+    // 더 보기 버튼 이미지, Label 위치 변경
+    @IBOutlet weak var moreButton: UIButton! {
+        didSet {
+            moreButton.transform = CGAffineTransform(scaleX: -1, y: 1)
+            moreButton.titleLabel?.transform = CGAffineTransform(scaleX: -1, y: 1)
+            moreButton.imageView?.transform = CGAffineTransform(scaleX: -1, y: 1)
+        }
+    }
+    
     // 가까운 장소 CollectionView 설정
     private let nearPlaces: [String] = ["유나네 집", "호준이네 집", "우리 집", "정재네 집", "수연이네 집"]
     private let nearPlaceImage: [String] = []
@@ -44,6 +53,35 @@ class HomeVC: UIViewController {
         }
     }
     
+    // MARK: - 인기 있는 장소 설정
+    @IBOutlet weak var hotPlaceLabel: UILabel! {
+        didSet {
+            let attributeHotPlaceText = "요즘 여기가 인기 있어요!".makeAttributedText([.font: UIFont.boldSystemFont(ofSize: 18)])
+            attributeHotPlaceText.addAttributes([.font: UIFont.systemFont(ofSize: 18)], range: ("요즘 여기가 인기 있어요!" as NSString).range(of: "요즘 여기가"))
+            hotPlaceLabel.attributedText = attributeHotPlaceText
+            hotPlaceLabel.sizeToFit()
+        }
+    }
+    
+    // 인기있는 장소 CollectionView 설정
+    private var hotPlaces: [String] = ["세림픽 돼지고기 집", "세림픽 요거트 집", "세림픽 커피집", "세림픽 소고기 집"]
+    
+    @IBOutlet weak var hotPlaceCollectionViewHeightConstraint: NSLayoutConstraint!
+    
+    func setHotPlaceCollectionViewHeight() {
+        let horizontalSpacing = view.frame.width / 23.44
+        let lineSpacing: CGFloat = 12
+        let cellWidth = hotPlaceCollectionView.frame.width - 2*horizontalSpacing
+        let cellHeight = cellWidth * 0.46
+        hotPlaceCollectionViewHeightConstraint.constant = cellHeight * CGFloat(hotPlaces.count) + lineSpacing * CGFloat((hotPlaces.count-1))
+    }
+    
+    @IBOutlet weak var hotPlaceCollectionView: UICollectionView! {
+        didSet {
+            hotPlaceCollectionView.dataSource = self
+            hotPlaceCollectionView.delegate = self
+        }
+    }
     
     // MARK: - ViewController Override 부분
     override func viewDidLoad() {
@@ -53,20 +91,34 @@ class HomeVC: UIViewController {
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
+        setHotPlaceCollectionViewHeight()
     }
 }
 
 extension HomeVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return nearPlaces.count
+        if collectionView == nearPlaceCollectionView { return nearPlaces.count }
+        else { return hotPlaces.count }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let homeNearPlaceCell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeNearPlaceCell.identifier, for: indexPath) as? HomeNearPlaceCell else { return UICollectionViewCell() }
+        if collectionView == nearPlaceCollectionView { return makeNearPlaceCell(collectionView, indexPath) }
+        else { return makeHotPlaceCell(collectionView, indexPath) }
+    }
+    
+    private func makeNearPlaceCell(_ collectionView: UICollectionView, _ indexPath: IndexPath) -> HomeNearPlaceCell {
+        guard let homeNearPlaceCell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeNearPlaceCell.identifier, for: indexPath) as? HomeNearPlaceCell else { return HomeNearPlaceCell() }
         homeNearPlaceCell.placeName = nearPlaces[indexPath.row]
         homeNearPlaceCell.layer.cornerRadius = homeNearPlaceCell.frame.width / 15.9
         homeNearPlaceCell.clipsToBounds = true
         return homeNearPlaceCell
+    }
+    
+    private func makeHotPlaceCell(_ collectionView: UICollectionView, _ indexPath: IndexPath) -> HotPlaceCell {
+        guard let hotPlaceCell = collectionView.dequeueReusableCell(withReuseIdentifier: HotPlaceCell.identifier, for: indexPath) as? HotPlaceCell else { return HotPlaceCell() }
+        hotPlaceCell.layer.cornerRadius = hotPlaceCell.frame.width / 17.15
+        hotPlaceCell.clipsToBounds = true
+        return hotPlaceCell
     }
 }
 
@@ -76,8 +128,15 @@ extension HomeVC: UICollectionViewDelegate {
 
 extension HomeVC: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.frame.height * 0.9
-        return CGSize(width: width, height: collectionView.frame.height)
+        if collectionView == nearPlaceCollectionView {
+            let width = collectionView.frame.height * 0.9
+            return CGSize(width: width, height: collectionView.frame.height)
+        } else {
+            let horizontalSpacing = view.frame.width / 23.44
+            let cellWidth = collectionView.frame.width - 2*horizontalSpacing
+            let cellHeight = cellWidth * 0.46
+            return CGSize(width: cellWidth, height: cellHeight)
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
